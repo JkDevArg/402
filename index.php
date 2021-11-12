3b2afd928894f51cda7fdce12cdadc09e97d70e4
JkDev <69984125+JkDevArg@users.noreply.github.com>
2021-11-12 04:06:31 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+    
+    $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $genNew = substr(str_shuffle($permitted_chars), 22, 22);
+    $web = 'https://chat.whatsapp.com/'.$genNew;
+    echo $web; //out -> https://chat.whatsapp.com/BMExBnAzj7XIo5X1Kyh2Fa
+
+    function curl($url) {
+        $ch = curl_init($url); // Inicia sesión cURL
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); // Configura cURL para devolver el resultado como cadena
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Configura cURL para que no verifique el peer del certificado dado que nuestra URL utiliza el protocolo HTTPS
+        $info = curl_exec($ch); // Establece una sesión cURL y asigna la información a la variable $info
+        curl_close($ch); // Cierra sesión cURL
+        return $info; // Devuelve la información de la función
+    }
+
+    $sitioweb = curl($web);  // Ejecuta la función curl escrapeando el sitio web https://devcode.la and regresa el valor a la variable $sitioweb
+    echo $sitioweb;
+?>
